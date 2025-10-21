2401a3d8f3dc0b3b1db64f22ee000dad917639b2
Dan  Landayan <dan.landayan@gmail.com>
2025-10-21 13:58:49 -0700

    Delete connectomics_gui directory

--- connectomics_gui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GUI for exploring FAFB segmentation synaptic partners."""
-
-from .annotations import CellTypeCacheWarning, fetch_cell_types
-from .app import ConnectomicsApp, main
-
-__all__ = ["ConnectomicsApp", "main", "fetch_cell_types", "CellTypeCacheWarning"]
